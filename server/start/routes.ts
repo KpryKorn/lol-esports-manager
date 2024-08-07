6e189ffab65856998da71225005156edbe3e95ff
KpryKorn <roffinisacha@gmail.com>
2024-08-07 16:37:55 +0200

    :sparkles: feat: ajout auth et protection des routes

--- server/start/routes.ts
+++ server/start/routes.ts
@@ -8,9 +8,37 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+const AuthController = () => import('#controllers/auth_controller')
+
+router
+  .group(() => {
+    // health check
+    router.get('/', async () => {
+      return { hello: 'world' }
+    })
+
+    // routes /user
+    router
+      .group(() => {
+        router.post('/register', [AuthController, 'register'])
+        router.post('/login', [AuthController, 'login'])
+        router.post('/logout', [AuthController, 'logout']).use(middleware.auth())
+
+        router
+          .get('/me', async ({ auth, response }) => {
+            try {
+              const user = auth.getUserOrFail()
+              return response.ok(user)
+            } catch (error) {
+              return response.unauthorized({
+                error: 'Unauthorized',
+              })
+            }
+          })
+          .use(middleware.auth())
+      })
+      .prefix('user')
+  })
+  .prefix('api')
